b67ab398e38710b0632124d27658a31713e402d9
acrylic-style <me@acrylicstyle.xyz>
2020-09-17 12:19:56 +0900

    maybe it looks good?

--- public/js/main.js
+++ public/js/main.js
@@ -6,14 +6,27 @@ snowStorm.vMaxX = 4;
 snowStorm.flakesMaxActive = 72;
 function registerUrl(id, url) {
   try {
-    document.getElementById(id).onclick = () => openUrl(url)
+    document.getElementById(id).onclick = () => openUrl(url);
   } catch (e) {
-    console.error(`Could not register URL(ID: ${id}, URL: ${url}): ${e.stack || e}`)
+    console.error(`Could not register URL(ID: ${id}, URL: ${url}): ${e.stack || e}`);
   }
 }
 function openUrl(url) {
-  const opened = window.open(url, '_blank')
-  opened.opener = null
+  const opened = window.open(url, '_blank');
+  opened.opener = null;
+}
+function promptOldPage() {
+  console.warn('why would you call unused function?');
+  Swal.fire({
+    title: 'Do you want to visit the old site?',
+    showCancelButton: true,
+    confirmButtonText: 'Yes',
+    cancelButtonText: 'No',
+  }).then(result => {
+    if (result.isConfirmed) {
+      location.href = 'https://acrylicstyle.xyz';
+    }
+  });
 }
 
 registerUrl('osu', 'https://osu.ppy.sh/users/13293262');
