c817de0e66c9565b20830b0984038c68ac959079
acrylic-style <me@acrylicstyle.xyz>
2020-09-17 15:16:52 +0900

    add video lol

--- public/js/main.js
+++ public/js/main.js
@@ -28,6 +28,41 @@ function promptOldPage() {
     }
   });
 }
+let confirmed = localStorage.getItem('confirmed') === 'true'
+let videoEnabled = localStorage.getItem('videoEnabled') === 'true'
+async function toggleVideo(newState) {
+  if (!confirmed) {
+    videoEnabled = false;
+    localStorage.setItem('videoEnabled', 'false');
+    const result = await Swal.fire({
+      title: 'Are you sure want to enable video? (It takes ~50MB!)',
+      showCancelButton: true,
+      confirmButtonText: 'Yes',
+      cancelButtonText: 'No',
+    })
+    if (result.isConfirmed) {
+      confirmed = true;
+      localStorage.setItem('confirmed', 'true')
+    }
+  }
+  if (!confirmed) return;
+  if (typeof(newState) !== 'undefined') videoEnabled = !newState
+  if (videoEnabled) {
+    document.getElementById('video').pause()
+    document.getElementById('video').classList.add('hidden')
+    document.getElementById('movieIcon').textContent = 'play_arrow'
+    videoEnabled = false;
+    localStorage.setItem('videoEnabled', 'false');
+  } else {
+    document.getElementById('video').classList.remove('hidden')
+    document.getElementById('video').setAttribute('src', 'images/background/night.mp4')
+    document.getElementById('video').play()
+    document.getElementById('movieIcon').textContent = 'pause'
+    videoEnabled = true;
+    localStorage.setItem('videoEnabled', 'true');
+  }
+}
+if (videoEnabled && confirmed) toggleVideo(true)
 
 registerUrl('osu', 'https://osu.ppy.sh/users/13293262');
 registerUrl('github', 'https://github.com/acrylic-style');
