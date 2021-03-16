2ca1f359099541185a1587c0a5dc9ba216db9c3d
acrylic-style <ilyy@outlook.jp>
2021-03-16 16:59:35 +0900

    send valid user-agent

--- public/api/repos.php
+++ public/api/repos.php
@@ -19,7 +19,7 @@ $c = curl_init();
 curl_setopt($c, CURLOPT_HTTPHEADER, ["Content-Type: application/json", "Authorization: token $token"]);
 curl_setopt($c, CURLOPT_URL, "https://api.github.com/user/repos?per_page=100&page=$page");
 curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($c, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+curl_setopt($c, CURLOPT_USERAGENT, 'acrylicstyle.xyz/1.0 https://github.com/acrylic-style/acrylicstyle.xyz');
 curl_setopt($c, CURLOPT_SSL_VERIFYPEER, 0);
 $out = curl_exec($c);
 curl_close($c);
