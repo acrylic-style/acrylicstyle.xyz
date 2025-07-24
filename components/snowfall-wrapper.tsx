88a0e3820f1ae92a165d1c980ee0a1aad495d12d
acrylic-style <me@acrylicstyle.xyz>
2025-07-24 22:34:36 +0900

    ssg yosi

--- components/snowfall-wrapper.tsx
+++ components/snowfall-wrapper.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import {Snowfall} from "@/react-snowfall/packages/react-snowfall";
+import {Snowfall} from "@/.react-snowfall/packages/react-snowfall";
 
 export function SnowfallWrapper() {
   return (
